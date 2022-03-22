6ad5532dc9c4437961635d99584d1e3dd8e558a4
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-03-22 14:33:02 +0000

    foir you my lover

---
+++ methods and returnas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProgramToAmos
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            double x;
+            double y;
+            double result;
+            
+            Console.WriteLine("Hello Amos please choose your fist number");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Now the second");
+            y = Convert.ToDouble(Console.ReadLine());
+            result = Multi(x, y);
+            
+            Console.WriteLine(result);
+        }
+
+        static double Multi(double x, double y)
+        {
+            double z = x * y;
+            return z;
+        }
+    }
+    
+}
